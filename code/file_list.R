14f388cb937bc71521493f54bcc0955cb3fa3e9e
igoldsteinh <61714888+igoldsteinh@users.noreply.github.com>
2020-10-21 14:03:35 -0700

    Fixing displaying archived posts

---
+++ code/file_list.R
@@ -0,0 +1,4 @@
+file_list<-c("analysis/index.Rmd", "analysis/about.Rmd", "analysis/license.Rmd")
+file_list <- c("analysis/jul24_aug28.Rmd", "analysis/jul18_aug22.Rmd", "analysis/jul11_aug15.Rmd",
+               "analysis/jun20_jul25.Rmd", "analysis/jul28_sept1.Rmd", "analysis/aug2_sept6.Rmd",
+               "analysis/aug7_sept11.Rmd", "analysis/aug12_sept16.Rmd")

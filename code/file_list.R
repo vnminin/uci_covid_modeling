bdb793f4c8bffa183c91659e70742a16607b0885
igoldsteinh <61714888+igoldsteinh@users.noreply.github.com>
2020-10-21 14:16:16 -0700

    really fixed the archived post problem

--- code/file_list.R
+++ code/file_list.R
@@ -1,4 +1,5 @@
 file_list<-c("analysis/index.Rmd", "analysis/about.Rmd", "analysis/license.Rmd")
-file_list <- c("analysis/jul24_aug28.Rmd", "analysis/jul18_aug22.Rmd", "analysis/jul11_aug15.Rmd",
+file_list <- c("analysis/index.Rmd", "analysis/about.Rmd", "analysis/license.Rmd", "analysis/jul24_aug28.Rmd", "analysis/jul18_aug22.Rmd", "analysis/jul11_aug15.Rmd",
                "analysis/jun20_jul25.Rmd", "analysis/jul28_sept1.Rmd", "analysis/aug2_sept6.Rmd",
-               "analysis/aug7_sept11.Rmd", "analysis/aug12_sept16.Rmd")
+               "analysis/aug7_sept11.Rmd", "analysis/aug12_sept16.Rmd",
+               "analysis/aug17_sep21.Rmd", "analysis/aug28_oct02.Rmd")

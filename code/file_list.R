05959a18f81b3b929e8efdac8d38a0e53a706aa0
igoldsteinh <61714888+igoldsteinh@users.noreply.github.com>
2020-11-02 21:43:30 -0800

    november 2nd update

--- code/file_list.R
+++ code/file_list.R
@@ -2,4 +2,5 @@ file_list<-c("analysis/index.Rmd", "analysis/about.Rmd", "analysis/license.Rmd")
 file_list <- c("analysis/index.Rmd", "analysis/about.Rmd", "analysis/license.Rmd", "analysis/jul24_aug28.Rmd", "analysis/jul18_aug22.Rmd", "analysis/jul11_aug15.Rmd",
                "analysis/jun20_jul25.Rmd", "analysis/jul28_sept1.Rmd", "analysis/aug2_sept6.Rmd",
                "analysis/aug7_sept11.Rmd", "analysis/aug12_sept16.Rmd",
-               "analysis/aug17_sep21.Rmd", "analysis/aug28_oct02.Rmd", "analysis/sept02_oct09.Rmd")
+               "analysis/aug17_sep21.Rmd", "analysis/aug28_oct02.Rmd", "analysis/sept02_oct09.Rmd",
+               "analysis/sept11_oct16.Rmd")

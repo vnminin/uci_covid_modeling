6d7c7aa4261a57853f187a4cdaaed77786fa0e8f
igoldsteinh <61714888+igoldsteinh@users.noreply.github.com>
2020-12-02 09:09:30 -0800

    situation report december 1

--- code/file_list.R
+++ code/file_list.R
@@ -4,4 +4,4 @@ file_list <- c("analysis/index.Rmd", "analysis/about.Rmd", "analysis/license.Rmd
                "analysis/aug7_sept11.Rmd", "analysis/aug12_sept16.Rmd",
                "analysis/aug17_sep21.Rmd", "analysis/aug28_oct02.Rmd", "analysis/sept02_oct09.Rmd",
                "analysis/sept11_oct16.Rmd", "analysis/sept18_oct23.Rmd", "analysis/sept25_oct30.Rmd",
-               "analysis/oct02_nov06.Rmd")
+               "analysis/oct02_nov06.Rmd", "analysis/oct11_nov15.Rmd")
